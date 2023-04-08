6bba0d568270f88f83083f5e0a05a4c823efa645
Shlok Datye <shlok@quant.is>
2023-04-08 12:59:16 +0200

    Add flake.nix.

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "streamly-lmdb";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        ghcVersion = "8107";
+        config = {};
+
+        overlays = [
+          (final: prev:
+            let
+              haskellPkgs = final.haskell.packages."ghc${ghcVersion}";
+            in {
+              myHaskellPkgs = haskellPkgs.override {
+                overrides = hfinal: hprev: {
+                  streamly-lmdb =
+                    final.haskell.lib.compose.addBuildDepends
+                      [final.pkgs.lmdb]
+                      (hfinal.callCabal2nix "streamly-lmdb" ./. {});
+                };
+              };
+
+              streamly-lmdb = final.myHaskellPkgs.streamly-lmdb;
+
+              myDevShell = final.myHaskellPkgs.shellFor {
+                packages = p: [p.streamly-lmdb];
+
+                buildInputs = [final.pkgs.lmdb];
+
+                nativeBuildInputs = [
+                  final.myHaskellPkgs.cabal-install
+                  final.myHaskellPkgs.haskell-language-server
+                ];
+              };
+            })
+        ];
+
+        pkgs = import nixpkgs { inherit config overlays system; };
+      in {
+        packages = {
+          default = pkgs.streamly-lmdb;
+          streamly-lmdb = pkgs.streamly-lmdb;
+        };
+        
+        devShells.default = pkgs.myDevShell;
+      });
+}
