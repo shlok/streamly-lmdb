d4307039fdc184c48777acce2cb6be158659d2a6
Shlok Datye <shlok@quant.is>
2023-06-14 16:05:49 +0200

    Parallelize test suite.
    (This became possible due to read-write concurrency support.)

--- test/TestSuite.hs
+++ test/TestSuite.hs
@@ -1,37 +1,22 @@
 module Main where
 
-import Streamly.External.LMDB
-  ( Database,
-    Environment,
-    ReadWrite,
-    defaultLimits,
-    getDatabase,
-    mapSize,
-    openEnvironment,
-    tebibyte,
-  )
-import qualified Streamly.External.LMDB.Tests (tests)
-import System.Directory (removeDirectoryRecursive)
-import System.Environment (setEnv)
-import System.IO.Temp (createTempDirectory, getCanonicalTemporaryDirectory)
-import Test.Tasty (TestTree, defaultMain, testGroup, withResource)
+import Streamly.External.LMDB.Channel
+import qualified Streamly.External.LMDB.Tests
+import Test.Tasty
 
 main :: IO ()
-main = do
-  setEnv "TASTY_NUM_THREADS" "1" -- Multiple tests use the same LMDB database.
+main =
   defaultMain $
     withResource
       ( do
-          tmpParent <- getCanonicalTemporaryDirectory
-          tmpDir <- createTempDirectory tmpParent "streamly-lmdb-tests"
-          env <- openEnvironment tmpDir $ defaultLimits {mapSize = tebibyte}
-          db <- getDatabase env Nothing
-          return (tmpDir, (db, env))
+          chan <- createChannel defaultChannelOptions
+          startChannel chan
+          return chan
       )
-      (\(tmpDir, _) -> removeDirectoryRecursive tmpDir)
-      (\io -> tests $ snd <$> io)
+      endChannel
+      tests
 
-tests :: IO (Database ReadWrite, Environment ReadWrite) -> TestTree
+tests :: IO Channel -> TestTree
 tests res =
   testGroup
     "Tests"
